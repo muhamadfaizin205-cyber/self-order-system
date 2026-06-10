0f78385a8f5a2570129dd1de797d1269c36ed21c
muhamadfaizin205-cyber <muhamadfaizin205@gmail.com>
2026-06-10 19:30:28 +0700

    Update supabase.js

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://lhcbbupqhpljhtcdrloy.supabase.co";
-const SUPABASE_KEY = "sb_publishable_uc3y6YCR6wSKCCEajArRgQ_U-1g0f_q";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoY2JidXBxaHBsamh0Y2RybG95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMTM4MDksImV4cCI6MjA5NjU4OTgwOX0.V5SuBRkV0lTjTtC2sZ5siCXOT8Zpapv2YGKbFAgTSLs";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
